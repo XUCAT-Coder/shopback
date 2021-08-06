756c48913534ff2b495024870d23f26c08aa586e
Coder_Monet <66582041+XUCAT-Coder@users.noreply.github.com>
2021-08-06 16:53:16 +0800

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,29 @@
+
+module.exports = {
+  outputDir: 'dark',
+  assetsDir:'static',
+  publicPath:'',
+  configureWebpack: {
+    resolve: {
+      alias: {
+        assets: '@/assets'
+      }
+    }
+  },
+  chainWebpack: (config) => {
+    config.when(process.env.NODE_ENV === 'production', (config) => {
+      config.entry('app').clear().add('./src/main-prod.js')
+      config.plugin('html').tap((args) => {
+        args[0].isProd = true
+        return args
+      })
+    })
+    config.when(process.env.NODE_ENV === 'development', (config) => {
+      config.entry('app').clear().add('./src/main-dev.js')
+      config.plugin('html').tap((args) => {
+        args[0].isProd = false
+        return args
+      })
+    })
+  }
+}
