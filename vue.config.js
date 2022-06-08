fc37576e1d7d21c7f32e231f4109097785fc9a53
Coder_Monet <66582041+XUCAT-Coder@users.noreply.github.com>
2022-06-08 14:17:35 +0800

    Add files via upload

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
-
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
 module.exports = {
-  assetsDir:'static',
+  assetsDir: 'static',
   configureWebpack: {
     resolve: {
       alias: {
@@ -8,6 +8,7 @@ module.exports = {
       }
     }
   },
+
   chainWebpack: (config) => {
     config.when(process.env.NODE_ENV === 'production', (config) => {
       config.entry('app').clear().add('./src/main-prod.js')
@@ -15,6 +16,15 @@ module.exports = {
         args[0].isProd = true
         return args
       })
+      config.plugin('compressionPlugin').use(
+        new CompressionWebpackPlugin({
+          // filename: "[path][base].gz[query]",
+          algorithm: 'gzip',
+          test: new RegExp('\\.(' + ['js', 'css'].join('|') + ')$'),
+          threshold: 10240,
+          minRatio: 0.8
+        })
+      )
     })
     config.when(process.env.NODE_ENV === 'development', (config) => {
       config.entry('app').clear().add('./src/main-dev.js')
